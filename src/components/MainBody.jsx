8ac81bc8a6a37ca989674a39dab5359201af294c
DerQh <64221410+DerQh@users.noreply.github.com>
2026-02-25 11:48:11 +0300

    Add MainBody component for marketplace app

---
+++ src/components/MainBody.jsx
@@ -0,0 +1,50 @@
+import "./App.css";
+
+function Body() {
+  return (
+    <>
+    <div>
+      <h1>DIRECT FROM THE FARM</h1>
+      <p>THE MARKETPLACE APP FOR LOCAL FOOD & FARMS</p>
+      <p>Discover and connect with local food and farms near you.</p>
+      <img src="" alt="app store logo" />
+      <img src="" alt="google play logo" />
+      <div>
+        <img src="" alt=" app phone screenshot" />
+      </div>
+    </div>
+    <div>
+      <div>
+        <h2>HOW IT WORKS</h2>
+        <p>
+          We are on a mission to empower local farms and strengthen local food supply chain across Kisumu amd it's environs.
+        </p>
+      </div>
+      <div>
+        <img src="" alt="buy image" />
+        <h2>Buy</h2>
+        <p>Search on a map using a keyword for produce that is on sale. Message the seller and arrange a pickup, meetup, or delivery.</p>
+      </div>
+      <div>
+        <img src="" alt="sell image" />
+        <h2>Sell</h2>
+        <p>Create a free listing to sell your homegrown produce, chicken eggs, supplies, plant trees, and more.</p>
+      </div>
+      <div>
+        <img src="" alt="grow image" />
+        <h2>Grow</h2>
+        <p>Connect with the consumer and wholesale buyers for your homegrown produce without the hassle of branding, a website, or advertising. </p>
+      </div>
+      <button>LEARN MORE </button>
+    </div>
+    <div>
+      LOCAL FARM INSTITUTIONAL PURCHASING
+      <p>Are you a food buyer for an institution, retailer,or restaurant? Join our wholesale program to source directly from a diverse network of hundreds of farms across Kisumu and its environs. </p>
+      <p>      If you are looking to buy local food for personla use, please download the Puodho app to find farms near you.</p>
+    </div>
+     
+    </>
+  );
+}
+
+export default Body;
