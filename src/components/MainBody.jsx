291b68e7e6ae592cd6fef975373c132b8776ef12
DerQh <64221410+DerQh@users.noreply.github.com>
2026-02-25 15:58:02 +0300

    body footer

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -40,7 +40,37 @@ function Body() {
     <div>
       LOCAL FARM INSTITUTIONAL PURCHASING
       <p>Are you a food buyer for an institution, retailer,or restaurant? Join our wholesale program to source directly from a diverse network of hundreds of farms across Kisumu and its environs. </p>
-      <p>      If you are looking to buy local food for personla use, please download the Puodho app to find farms near you.</p>
+      <p>If you are looking to buy local food for personla use, please download the Puodho app to find farms near you.</p>
+
+       </div>
+     <div>
+        "Puodho has resulted in both professional and personal relationships with increible customes an people in my local community. Not only have I been able to \n network and make sales but I never eaten better or cleaner in my life! I downloaded it when I was the only producer withi 50 kms and now there are ovr 30 in my village alone! I can't wait to see what else Puodo has up their sleeve!" - Jeff Ondoro. 
+     </div>
+     <div>
+      <div>
+        <img src="" alt="logo  " />
+        <h1>FARMFRESH</h1>
+      </div>
+      <p>
+        Welcome to fARMfRESH! Our mission is to cnnectt communities with local food sources. Buy and sell garden and farm produce, backyar chicken eggs, supplies, and many more. 
+      </p>
+      <div>
+        <a href="">How it Works</a>
+        <a href="">About us</a>
+        <a href="">FAQ</a>
+      </div>
+      <input type="text" placeholder="Email" />
+      <button>Sign Up for News & Updates </button>
+      <p>support@famrfresh.com</p>
+      <div>
+        <img src="" alt="facebook logo" />
+        <img src="" alt="x logo" />
+        <img src="" alt="instagram logo" />
+        <img src="" alt="pintrest logo" />
+      </div>
+      <div>
+        <p>Privacy policy | Terms and Conditions | Cookie Policy | Refunds and Returns</p>
+      </div>
     </div>
      
     </>
